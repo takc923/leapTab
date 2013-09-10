a3d61432732579c2d61f518de027f61ee97f1942
takc923 <takc923@gmail.com>
2013-09-11 03:28:34 +0900

    you type t, the tabs titles will be changed.

--- background.js
+++ background.js
@@ -1,6 +1,13 @@
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         chrome.windows.getCurrent({populate: true},function(win){
-            _.each(win.tabs, function(tab){tab.favIconUrl = "https://wri.pe/favicon.ico";});
+            var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+            for (var i = 0;i < alphabet.length || i < win.tabs.length; i++) {
+                chrome.tabs.sendMessage(win.tabs[i].id, {
+                    action    : "change",
+                    faviconUrl: "http://developer.chrome.com/favicon.ico",
+                    title     : "[" + alphabet[i] + "] " + win.tabs[i].title
+                });
+            }
         });
     });
