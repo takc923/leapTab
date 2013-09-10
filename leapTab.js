4cc1ada97b4f283d6b19dd88261f1b6c49d05537
Takeshi Kunimi <takc923@gmail.com>
2013-09-11 03:28:34 +0900

    you type t, the tabs titles will be changed.

--- leapTab.js
+++ leapTab.js
@@ -1,7 +1,27 @@
+var originalFaviconUrl;
+var originalTitle;
 window.addEventListener("load", function(){
+    originalTitle = document.title;
+    originalFaviconUrl = getFavIconUrl();
     document.addEventListener("keypress", function(evt){
         if(evt.keyCode == 116){
-            chrome.runtime.sendMessage({message: "message_content"});
+            chrome.runtime.sendMessage({
+                action : "change",
+                message: "message_content"
+            });
         }
     });
+    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+        favicon.change(request.faviconUrl, request.title);
+    });
 });
+
+function getFavIconUrl() {
+    var linkDoms = document.head.getElementsByTagName("link");
+    for(var i = 0; i < linkDoms.length; i++) {
+        if (linkDoms[i].rel.indexOf("icon") != -1) {
+            return linkDoms[i].href;
+        }
+    }
+    return location.origin + "/favicon.ico";
+}
