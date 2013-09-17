9976171012b35c79c5c84e9161241693e3e99308
Takeshi Kunimi <takc923@gmail.com>
2013-09-18 03:15:56 +0900

    change logic related reset.

--- background.js
+++ background.js
@@ -1,6 +1,7 @@
 var vimiumBinds = "bdfghijklmnoprtuxyzBFGHJKLNOPTX0123456789";
 var alphanumeric = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var bindedKeys = "";
+var originalTabs;
 for (var i = 0; i < alphanumeric.length; i++) {
     if (vimiumBinds.indexOf(alphanumeric[i]) == -1) {
         bindedKeys += alphanumeric[i];
@@ -22,25 +23,19 @@ chrome.tabs.onActivated.addListener(reset);
 function prepareLeap() {
     // TODO: 今いるタブはスキップしたい
     chrome.windows.getCurrent({populate: true}, function(win) {
+        originalTabs = win.tabs;
         for (var i = 0; i < bindedKeys.length && i < win.tabs.length; i++) {
-            chrome.tabs.sendMessage(win.tabs[i].id, {
-                action    : "change",
-                args : {
-                    character : bindedKeys[i]
-                }
-            });
+            changeFavicon(win.tabs[i].id, getAlphanumericImageUrl(bindedKeys[i]));
         }
     });
 }
 
 function reset() {
-    chrome.windows.getCurrent({populate: true},function(win){
-        for (var i = 0;i < win.tabs.length; i++) {
-            chrome.tabs.sendMessage(win.tabs[i].id, {
-                action    : "reset"
-            });
-        }
-    });
+    if (! originalTabs) return;
+    for (var i = 0;i < originalTabs.length; i++) {
+        changeFavicon(originalTabs[i].id, originalTabs[i].favIconUrl);
+    }
+    originalTabs = null;
 }
 
 function leap(code) {
@@ -48,4 +43,23 @@ function leap(code) {
         if (bindedKeys.indexOf(String.fromCharCode(code)) >= win.tabs.length) reset();
         chrome.tabs.update(win.tabs[bindedKeys.indexOf(String.fromCharCode(code))].id, {active: true});
     });
+}
+
+function changeFavicon(tabId, favIconUrl) {
+    chrome.tabs.sendMessage(tabId, {
+        action    : "change",
+        favIconUrl: favIconUrl
+    });
+}
+
+function getAlphanumericImageUrl(character) {
+    var prefix = "";
+    if(character.search(/^[0-9]$/) == 0) {
+        prefix = "";
+    } else if (character.search(/^[a-z]$/) == 0) {
+        prefix = "s_";
+    } else if (character.search(/^[A-Z]$/) == 0) {
+        prefix = "c_";
+    }
+    return chrome.extension.getURL("favicon/" + prefix + character + ".ico");
 }
--- leapTab.js
+++ leapTab.js
@@ -3,7 +3,6 @@
 // 2. localstrageに入れる
 // 3. option pageで指定
 var vimiumBinds = "bdfghijklnmoprtuxyzBFGHJKLNOPTX0123456789";
-var originalFavIconUrl;
 // TODO: flag管理じゃなくて、faviconのurlを見て判断すれば良いのでは
 var beforeLeapFlag = false;
 var alphanumeric = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -16,7 +15,6 @@ for (var i = 0; i < alphanumeric.length; i++) {
 
 window.addEventListener("load", function(){
     // TODO: resetするとき直前のfaviconにした方がいい
-    originalFavIconUrl = getFavIconUrl();
     document.addEventListener("keydown", function(evt){
         if (document.activeElement.tagName != "BODY")  return;
         if(! beforeLeapFlag && evt.keyCode == 65 && ! evt.shiftKey) {
@@ -38,50 +36,21 @@ window.addEventListener("load", function(){
             });
         }
     });
-
-    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-        switch (request.action) {
-            case "change" : change(request.args); break;
-            case "reset"       : reset();       break;
-        }
-    });
 });
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    switch (request.action) {
+    case "change" : favicon.change(request.favIconUrl); break;
+    case "reset"       : reset();       break;
+    }
+});
+
 
 // TODO: 名前微妙 isEnableKeyとか有効なキーかどうか的な名前のほうが良さそう
 function isBinded(code) {
     return bindedKeys.indexOf(String.fromCharCode(code)) != -1;
 }
 
-function change(args) {
-    favicon.change(getAlphanumericImageUrl(args.character));
-}
-
-function reset() {
-    favicon.change(originalFavIconUrl);
-}
-
 function isAlphabet(code) {
     return String.fromCharCode(code).search(/^[a-zA-Z]$/) == 0;
-}
-
-function getFavIconUrl() {
-    var linkDoms = document.head.getElementsByTagName("link");
-    for(var i = 0; i < linkDoms.length; i++) {
-        if (linkDoms[i].rel.search(/^\s*(shortcut)?\s*icon\s*(shortcut)?\s*$/i) != -1) {
-            return linkDoms[i].href;
-        }
-    }
-    return location.origin + "/favicon.ico";
-}
-
-function getAlphanumericImageUrl(character) {
-    var prefix = "";
-    if(character.search(/^[0-9]$/) == 0) {
-        prefix = "";
-    } else if (character.search(/^[a-z]$/) == 0) {
-        prefix = "s_";
-    } else if (character.search(/^[A-Z]$/) == 0) {
-        prefix = "c_";
-    }
-    return chrome.extension.getURL("favicon/" + prefix + character + ".ico");
 }
