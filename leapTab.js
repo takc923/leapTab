c492bcfa56a2554beb539da388ee65c780e72b33
takc923 <takc923@gmail.com>
2013-10-17 03:09:25 +0900

    add comments

--- leapTab.js
+++ leapTab.js
@@ -128,6 +128,9 @@ function isPrefixEvent(evt) {
 }
 
 // if logicを他のところに移したらもっとシンプルになるのでは
+// faviconのあるなしの判断を自前でやるのではなく、chrome APIに頼るべき
+// ↓ここみたいに無効なfaviconを指定してるとバグる
+// http://www.getproxy.jp/
 function setIconLinkIfNotExists(callback) {
     if (document.head.querySelectorAll("link[rel~=icon]").length > 0) {
         callback();
